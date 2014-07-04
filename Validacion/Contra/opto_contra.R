f692546ec6e1a00f03ad9d4d575b5989b313d4b1
Ian Castillo Rosales <carian2996@gmail.com>
2014-07-03 20:08:41 -0500

    Version 1.1

--- Validacion/Contra/opto_contra.R
+++ Validacion/Contra/opto_contra.R
@@ -1,11 +1,11 @@
 # Ian Castillo Rosales (BANXICO\T41348)
-# Gerencia de Información del Sistema Financiero
-# Subgerencia de Información de Moneda Extranjera y Derivados
+# Gerencia de Informacion del Sistema Financiero
+# Subgerencia de Informacion de Moneda Extranjera y Derivados
 # 
-# Validación de información para operaciones con opciones
-# 230614 - 300414
+# Validacion de informacion para operaciones con opciones
+# 090614 - 030714
 
-opto <- function(ruta){
+opto_contra <- function(ruta){
       
       # ENTRADA
       # ruta = Ruta donde se encuentran los datos para los calculos
@@ -17,9 +17,9 @@ opto <- function(ruta){
       # opto_contra_[fecha].dbf - Archivo tipo .dbf con los resultados
       
       # ===== Librerias y directorios =====
-      setwd(paste(ruta, "/OPTO/", sep="")) # ¿Dónde están mis datos?
+      setwd(paste(ruta, "OPTO/", sep="")) # Donde estan mis datos?
       library(foreign) # Libreria necesaria para cargar los datos
-      options(scipen=999, digits=5)
+      options(scipen=999, digits=8)
       
       # ===== Carga de datos =====
       data <- read.dbf("opto.dbf", as.is=T)
@@ -31,19 +31,14 @@ opto <- function(ruta){
       
       # ===== Codigo ====
       # apply(data, 2, function(x) any(is.na(x)))
+      
       # Colocamos los registros que contengan casos incompletos (con NA's)
-      raros <- data[!complete.cases(data$TIP_INSTI, data$FE_CON_OPE, data$FE_VEN_OPE, 
-                                    data$MDO, data$TIP_OPE, data$OPC, data$CLASE_OPE,
-                                    data$C_IMP_BASE, data$MDA_IMP, data$FE_VEN_ORI, 
-                                    data$CONSEC_CF, data$ID_CF), ]
+      raros <- data[!complete.cases(data[, -c(3, 12, 13, 15)]), ]
       # Escogemos los registros que contengan casos completos (sin NA's)
-      data <- data[complete.cases(data$TIP_INSTI, data$FE_CON_OPE, data$FE_VEN_OPE, 
-                                  data$MDO, data$TIP_OPE, data$OPC, data$CLASE_OPE,
-                                  data$C_IMP_BASE, data$MDA_IMP, data$FE_VEN_ORI, 
-                                  data$CONSEC_CF, data$ID_CF), ]
+      data <- data[complete.cases(data[, -c(3, 12, 13, 15)]), ]
       # Arrojamos un mensaje en caso de que existan casos incompletos
       if(nrow(raros)!=0){
-            message("¡Existen registros incompletos!")
+            message("¡Existen registros incompletos en OPTO Contra!")
       }
       
       # ===== Tipo de Institucion =====
@@ -90,17 +85,17 @@ opto <- function(ruta){
       data$TIPO_ENTE <- NA
       data$RESI <- NA
       
-      data$TIPO_ENTE[!is.na(data$CONT)] <- clave_deri$tipo_ente[match(data$CONT[!is.na(data$CONT)], clave_deri$clave_deri)]
-      data$RESI[!is.na(data$CONT)] <- clave_deri$residencia[match(data$CONT[!is.na(data$CONT)], clave_deri$clave_deri)]
+      data$TIPO_ENTE <- clave_deri$tipo_ente[match(data$CONT, clave_deri$clave_deri)]
+      data$RESI <- clave_deri$residencia[match(data$CONT, clave_deri$clave_deri)]
       
       # ===== Sector =====
       data$SECTOR <- NA
       
-      data$SECTOR[data$TIPO_ENTE==4 &data$RESI=="MX" ] <- "Bancos Múltiples"
+      data$SECTOR[data$TIPO_ENTE==4 &data$RESI=="MX" ] <- "Bancos Multiples"
       data$SECTOR[data$TIPO_ENTE==5 &data$RESI=="MX" ] <- "Bancos de Desarrollo"
       data$SECTOR[data$TIPO_ENTE==6 &data$RESI=="MX" ] <- "Casas de Bolsa"
       
-      data$SECTOR[(data$TIPO_ENTE==19  | data$TIPO_ENTE==48) & data$RESI=="MX" ] <- "Sociedades y Fondos de Inversión"
+      data$SECTOR[(data$TIPO_ENTE==19  | data$TIPO_ENTE==48) & data$RESI=="MX" ] <- "Sociedades y Fondos de Inversion"
       data$SECTOR[(data$TIPO_ENTE==20  | data$TIPO_ENTE==37) & data$RESI=="MX" ] <- "SIEFORES"
       data$SECTOR[(data$TIPO_ENTE==38  | data$TIPO_ENTE==46) & data$RESI=="MX" ] <- "SOFOMES"
       
@@ -110,40 +105,45 @@ opto <- function(ruta){
       
       data$SECTOR[(data$TIPO_ENTE==43  | data$TIPO_ENTE==49) & data$RESI=="MX" ] <- "Gobiernos y Entidades Estatales o Municipales"
       
-      data$SECTOR[data$TIPO_ENTE==25 & data$RESI=="MX" ] <- "Personas físicas residentes en México"
+      data$SECTOR[data$TIPO_ENTE==25 & data$RESI=="MX" ] <- "Personas fisicas residentes en Mexico"
       
       data$SECTOR[(data$TIPO_ENTE==17  | data$TIPO_ENTE==26 | data$TIPO_ENTE==45) & data$RESI=="MX" ] <- "Empresas Privadas No Financieras"
       
       data$SECTOR[(data$TIPO_ENTE==4  | data$TIPO_ENTE==31) & data$RESI=="US" ] <- "Bancos comerciales en E.U.A."
-      data$SECTOR[(data$TIPO_ENTE==4  | data$TIPO_ENTE==31) & !is.na(match(data$RESI, c("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB", "VG"))) ] <- "Bancos comerciales en la Unión Europea"
-      data$SECTOR[(data$TIPO_ENTE==4  | data$TIPO_ENTE==31) & !is.na(match(data$RESI, c("AR", "BO", "BR", "CL", "CO", "CU", "DO", "EC", "SV", "GT", "HN", "NI", "PA", "PY", "PE", "PR", "UY", "VE"))) ] <- "Bancos comerciales en América Latina"
-      data$SECTOR[(data$TIPO_ENTE==4  | data$TIPO_ENTE==31) & !is.na(match(data$RESI, c("BS", "CA", "CH", "ND", "KY", "AN", "AU", "HK", "SG"))) ] <- "Bancos comerciales en otras regiones y países"
+      data$SECTOR[(data$TIPO_ENTE==4  | data$TIPO_ENTE==31) & !is.na(match(data$RESI, c("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB", "VG"))) ] <- "Bancos comerciales en la Union Europea"
+      data$SECTOR[(data$TIPO_ENTE==4  | data$TIPO_ENTE==31) & !is.na(match(data$RESI, c("AR", "BO", "BR", "CL", "CO", "CU", "DO", "EC", "SV", "GT", "HN", "NI", "PA", "PY", "PE", "PR", "UY", "VE"))) ] <- "Bancos comerciales en America Latina"
+      data$SECTOR[(data$TIPO_ENTE==4  | data$TIPO_ENTE==31) & !is.na(match(data$RESI, c("BS", "CA", "CH", "ND", "KY", "AN", "AU", "HK", "SG"))) ] <- "Bancos comerciales en otras regiones y paises"
       
       data$SECTOR[!is.na(match(data$MDO, c("CBE", "CME", "ICE", "NYS", "NYM", "CBT")))] <- "Mercados Reconocidos en E.U.A"
-      data$SECTOR[!is.na(match(data$MDO, c("LFF", "ASE", "EUX", "MEF"))) ] <- "Mercados Reconocidos en la Unión Europea"
-      data$SECTOR[data$MDO=="BMF"] <- "Mercados Reconocidos en América Latina"
+      data$SECTOR[!is.na(match(data$MDO, c("LFF", "ASE", "EUX", "MEF"))) ] <- "Mercados Reconocidos en la Union Europea"
+      data$SECTOR[data$MDO=="BMF"] <- "Mercados Reconocidos en America Latina"
       data$SECTOR[is.na(data$TIP_CONT) & is.na(match(data$MDO, c("CBE", "CME", "ICE", "NYS", "NYM", "CBT", "LFF", "ASE", "EUX", "MEF", "BMF"))) ] <- "Otras regiones o paises"
       
       data$SECTOR[!is.na(match(data$TIPO_ENTE, c(10,11,50,51,53))) & data$RESI=="US" ] <- "Otras entidades financieras en E.U.A."
-      data$SECTOR[!is.na(match(data$TIPO_ENTE, c(10,11,50,51,53))) & !is.na(match(data$RESI, c("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB", "VG"))) ] <- "Otras entidades financieras en la Unión Europea"
-      data$SECTOR[!is.na(match(data$TIPO_ENTE, c(10,11,50,51,53))) & !is.na(match(data$RESI, c("AR", "BO", "BR", "CL", "CO", "CU", "DO", "EC", "SV", "GT", "HN", "NI", "PA", "PY", "PE", "PR", "UY", "VE"))) ] <- "Otras entidades financieras en América Latina"
+      data$SECTOR[!is.na(match(data$TIPO_ENTE, c(10,11,50,51,53))) & !is.na(match(data$RESI, c("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB", "VG"))) ] <- "Otras entidades financieras en la Union Europea"
+      data$SECTOR[!is.na(match(data$TIPO_ENTE, c(10,11,50,51,53))) & !is.na(match(data$RESI, c("AR", "BO", "BR", "CL", "CO", "CU", "DO", "EC", "SV", "GT", "HN", "NI", "PA", "PY", "PE", "PR", "UY", "VE"))) ] <- "Otras entidades financieras en America Latina"
       data$SECTOR[!is.na(match(data$TIPO_ENTE, c(10,11,50,51,53))) & !is.na(match(data$RESI, c("BS", "CA", "CH", "ND", "KY", "AN", "AU", "HK", "SG"))) ] <- "Otras entidades financieras extranjeras"
       
       data$SECTOR[data$TIPO_ENTE==47 & data$RESI=="US" ] <- "Gobiernos Federal y Local de los E.U.A."
-      data$SECTOR[data$TIPO_ENTE==47 & !is.na(match(data$RESI, c("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB", "VG"))) ] <- "Gobiernos Federal y Local de la Unión Europea"
-      data$SECTOR[data$TIPO_ENTE==47 & !is.na(match(data$RESI, c("AR", "BO", "BR", "CL", "CO", "CU", "DO", "EC", "SV", "GT", "HN", "NI", "PA", "PY", "PE", "PR", "UY", "VE"))) ] <- "Gobiernos Federal y Local en América Latina"
-      data$SECTOR[data$TIPO_ENTE==47 & !is.na(match(data$RESI, c("BS", "CA", "CH", "ND", "KY", "AN", "AU", "HK", "SG"))) ] <- "Gobiernos Federal y Local en otras regiones y países"
+      data$SECTOR[data$TIPO_ENTE==47 & !is.na(match(data$RESI, c("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB", "VG"))) ] <- "Gobiernos Federal y Local de la Union Europea"
+      data$SECTOR[data$TIPO_ENTE==47 & !is.na(match(data$RESI, c("AR", "BO", "BR", "CL", "CO", "CU", "DO", "EC", "SV", "GT", "HN", "NI", "PA", "PY", "PE", "PR", "UY", "VE"))) ] <- "Gobiernos Federal y Local en America Latina"
+      data$SECTOR[data$TIPO_ENTE==47 & !is.na(match(data$RESI, c("BS", "CA", "CH", "ND", "KY", "AN", "AU", "HK", "SG"))) ] <- "Gobiernos Federal y Local en otras regiones y paises"
       
       data$SECTOR[data$TIPO_ENTE==28 & data$RESI=="US" ] <- "Empresas privadas no financieras en E.U.A."
-      data$SECTOR[data$TIPO_ENTE==28 & !is.na(match(data$RESI, c("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB", "VG"))) ] <- "Empresas privadas no financieras en la Unión Europea"
-      data$SECTOR[data$TIPO_ENTE==28 & !is.na(match(data$RESI, c("AR", "BO", "BR", "CL", "CO", "CU", "DO", "EC", "SV", "GT", "HN", "NI", "PA", "PY", "PE", "PR", "UY", "VE"))) ] <- "Empresas privadas no financieras en América Latina"
-      data$SECTOR[data$TIPO_ENTE==28 & !is.na(match(data$RESI, c("BS", "CA", "CH", "ND", "KY", "AN", "AU", "HK", "SG"))) ] <- "Empresas privadas no financieras en otras regiones y países"
+      data$SECTOR[data$TIPO_ENTE==28 & !is.na(match(data$RESI, c("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB", "VG"))) ] <- "Empresas privadas no financieras en la Union Europea"
+      data$SECTOR[data$TIPO_ENTE==28 & !is.na(match(data$RESI, c("AR", "BO", "BR", "CL", "CO", "CU", "DO", "EC", "SV", "GT", "HN", "NI", "PA", "PY", "PE", "PR", "UY", "VE"))) ] <- "Empresas privadas no financieras en America Latina"
+      data$SECTOR[data$TIPO_ENTE==28 & !is.na(match(data$RESI, c("BS", "CA", "CH", "ND", "KY", "AN", "AU", "HK", "SG"))) ] <- "Empresas privadas no financieras en otras regiones y paises"
       
-      data$SECTOR[data$TIPO_ENTE==27] <- "Personas físicas residentes en el extranjero"
+      data$SECTOR[data$TIPO_ENTE==27] <- "Personas fisicas residentes en el extranjero"
       
       # ===== WRITE =====
-      # Escribe el cuadro (.dbf) en el directorio de trabajo
+      # Escribe el cuadro en el directorio de trabajo
       write.dbf(data, paste("opto_contra_", format(Sys.Date()[1], "%d%m%Y"), ".dbf", sep=""))
       
-      data
+      if(nrow(raros)!=0){
+            resultado <- list(cuadro=data, raros=raros)
+            invisible(resultado)
+      } else{
+            invisible(data)
+      }
 }
