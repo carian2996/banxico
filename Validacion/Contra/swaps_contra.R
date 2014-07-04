f7223d29acaef5efa9915a6b3b128d0cc17c4f4e
Ian Castillo Rosales <carian2996@gmail.com>
2014-07-03 20:08:50 -0500

    Version 1.1

--- Validacion/Contra/swaps_contra.R
+++ Validacion/Contra/swaps_contra.R
@@ -1,21 +1,21 @@
 # Ian Castillo Rosales (BANXICO\T41348)
-# Gerencia de Información del Sistema Financiero
-# Subgerencia de Información de Moneda Extranjera y Derivados
+# Gerencia de Informacion del Sistema Financiero
+# Subgerencia de Informacion de Moneda Extranjera y Derivados
 # 
-# Validación de información para operaciones con swaps (contra)
+# Validacion de informacion para operaciones con swaps (contra)
 # 230614 - 300414
 
 swaps_contra_junta <- function(ruta){
       ruta="/Volumes/IAN/Estadisticas/Contraparte/"
       # ENTRADA
-      # ruta = Ruta donde se encuentran los archivos .R para los cálculos
+      # ruta = Ruta donde se encuentran los archivos .R para los calculos
       
       # SALIDA
       # swaps_contra_[fecha].dbf - Archivo tipo .dbf con los resultados
       
       # ===== Librerias y directorios =====
       library(foreign) # Libreria necesaria para cargar los datos
-      options(scipen=999, digits=10) # Quita la notación exp y trunca a 4 decimales
+      options(scipen=999, digits=8) # Quita la notacion exp y trunca a 4 decimales
       
       # ===== Funciones =====
       source(paste(ruta, "R/swaps1_contra.R", sep=""))
@@ -67,8 +67,8 @@ swaps_contra_junta <- function(ruta){
       swaps[swaps$SECCION=="III", columnas] <- s3[, columnas]
       swaps[swaps$SECCION=="VI", columnas] <- s4[, columnas]
       
+      # Escribe el cuadro (.xlsx) en el directorio de trabajo
       write.dbf(swaps, paste("swaps_contra_", format(Sys.Date()[1], "%d%m%Y"), ".dbf", sep=""))
       
       swaps
-      # apply(swaps, 2, function(x) any(is.na(x)))
 }
