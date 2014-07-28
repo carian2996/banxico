d703ef6d5b821a1aef0ae889ca6d428864574811
Ian Castillo Rosales <carian2996@gmail.com>
2014-07-28 11:11:19 -0500

    Uso del paquete plyr

--- find_packages.R
+++ find_packages.R
@@ -19,8 +19,10 @@
 # La diferencia entre las fechas de vencimiento de cada
 # derivado son iguales.
 
-find_packages <- function(ruta, file_name) {
-
+find_packages <- function(ruta, file_name, n_records, n_fields) {
+      ruta = "/Volumes/IAN"
+      file_name = "base_paquetes_divisas.csv"
+      
 	# ENTRADA
 	# ruta = Especifica la ruta del directorio donde se encuentran los datos
 	# file_name = Especifica el nombre del archivo .dbf
@@ -30,15 +32,62 @@ find_packages <- function(ruta, file_name) {
 
 
 	# ===== Librerias y directorios =====
-      setwd(paste(ruta)) # Donde estan mis datos?
-      library(foreign) # Libreria necesaria para cargar los datos
-      options(scipen=999, digits=8) # Opciones de formato
+      setwd(ruta) # Donde estan mis datos?
       
       # ===== Carga de datos =====
-      data <- read.dbf(file_name, as.is=T) # Lee los datos
-      data <- data[data$, ] # Quita los paquetes que ya fueron identificados
-      gc() # Libera espacio en la memoria
-
+      data <- read.csv(file_name, quote = "", as.is = T)
+      data$INSTI <- sprintf("%06d", data[, "INSTI"])
+      data$MDA_IMP <- sprintf("%02d", data[, "MDA_IMP"])
+      data$SUBY <- sprintf("%03d", data[, "SUBY"])
+      data$CONT <- sprintf("%06d", data[, "CONT"])
+      data <- cbind(seq(nrow(data)), data)
+      names(data)[1] <- "ID"
+      
       # Busca en que columna hay por lo menos un valor vacio
-      apply(data, 2, function(x) any(is.na(x)))
+      if(any(apply(data, 2, function(x) any(is.na(x))))){
+            message("Existen registros incompletos la base")
+      }
+      
+      precios <- subset(count(data, "PREC_EJE"), freq >= 2)
+      new_data <- match_df(data, precios, on="PREC_EJE")
+
+      importe_base <- subset(count(new_data, "IMP_BASE"), freq >= 2)
+      new_data <- match_df(new_data, importe_base, on="IMP_BASE")
+      
+      institucion <- subset(count(new_data, "INSTI"), freq >= 2)
+      new_data <- match_df(new_data, institucion, on="INSTI")
+      
+      fecha_con <- subset(count(new_data, "FE_CON_OPE"), freq >= 2)
+      new_data <- match_df(new_data, fecha_con, on="FE_CON_OPE")
+      
+      subyacentes <- sort(unique(data$SUBY))
+      list_packages <- list()
+      
+      for(i in seq_along(subyacentes)){
+            
+            new_data <- data[data$SUBY == subyacentes[i], ]
+            precios <- sort(unique(new_data$PREC_EJE))
+            precios <- precios[table(new_data$PREC_EJE) >= 2]
+            new_data <- new_data[new_data$PREC_EJE %in% precios, ]
+            
+            imp_base <- sort(unique(new_data$IMP_BASE))
+            imp_base <- imp_base[table(new_data$IMP_BASE) >= 2]
+            new_data <- new_data[new_data$IMP_BASE %in% imp_base, ]
+            
+            insti <- sort(unique(new_data$INSTI))
+            insti <- insti[table(new_data$INSTI) >= 2]
+            new_data <- new_data[new_data$INSTI %in% insti, ]
+            
+            fecha_con <- sort(unique(new_data$FE_CON_OPE))
+            fecha_con <- fecha_con[table(new_data$FE_CON_OPE) >= 2]
+            new_data <- new_data[new_data$FE_CON_OPE %in% fecha_con, ]
+            
+            list_packages[[i]] <- new_data
+      }
+      
+      names(list_packages) <- subyacentes
+      
+      new_data <- list_packages[subyacentes[8]][[1]]
+      dlply(new_data, .(new_data$PREC_EJE), nrow)
+
 }
